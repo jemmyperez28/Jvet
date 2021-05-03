637fb0b1e26a3cac1fa3d13290727056b66834c4
jemmyperez28 <jemmyperez28@gmail.com>
2021-05-03 00:53:44 -0500

    pagina principal

--- blueprints/anon_bp.py
+++ blueprints/anon_bp.py
@@ -1,12 +1,22 @@
 from flask import Blueprint , render_template , request , session , redirect , url_for , flash , current_app 
 import flask 
 from config.db import db 
-from models import Vet
+from models import Vet , Productos , Servicios , Empleado
 from sqlalchemy import exc , desc , func
 anon_bp = Blueprint('anon_bp',__name__)
 
-@anon_bp.route('/', methods=['GET','POST'])
-def index_anon():
-    datos = Vet.query.all()
-    return render_template("/app/anon_index.html",datos=datos)
+@anon_bp.route('/', methods=['GET','POST'], defaults={'nom':None})
+@anon_bp.route('/<nom>', methods=['GET','POST'] )
+def index_anon(nom):
+    nombre_vet = nom 
+    if nombre_vet is None:
+        datos = Vet.query.all()
+        return render_template("/app/anon_index.html",datos=datos)
+    else:
+        datos = Vet.query.filter_by(nombre_unico=nombre_vet).first()
+        productos = Productos.query.filter_by(idvet=datos.idvet).all()
+        servicios = Servicios.query.filter_by(idvet=datos.idvet).all()
+        empleados = Empleado.query.filter_by(idvet=datos.idvet).all()
+        return render_template("/app/anon_vet.html",datos=datos , productos=productos,servicios=servicios,empleados=empleados)
+
 
